6e299892486967834d3470c94973ded849021f8d
shin katsumaru <ms.katsumaru@gmail.com>
2017-12-25 22:27:59 +0900

    first commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+DOT_DIRECTORY=${HOME}/git/dotfiles
+
+for f in .??*
+do
+  [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".gitignore" ]] && continue
+  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
